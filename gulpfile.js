483d6a00af8528a8b844172cfdc772caae298eee
yubin <qybbqybb@gmail.com>
2016-12-01 22:24:59 -0800

    laravel setup

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var elixir = require('laravel-elixir');
+elixir.config.js.browserify.watchify.options.poll = true;
+
+// elixir.config.js.browserify.transformers.push({
+//     name: 'vueify'
+// });
+require('laravel-elixir-vueify');
+
+/*
+ |--------------------------------------------------------------------------
+ | Elixir Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Elixir provides a clean, fluent API for defining some basic Gulp tasks
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for our application, as well as publishing vendor resources.
+ |
+ */
+
+elixir(function(mix) {
+    mix.sass('app.scss');
+
+    mix.browserify('admin.js')
+    mix.browserify('home.js')
+    mix.version(['public/js/admin.js', 'public/js/home.js']);
+});
